0e9f29d8e266c1a98c05041c837cc6c0261a2cf9
oshxia <18010705@usc.edu.ph>
2025-11-27 14:41:10 +0800

    Update StartMenu.java

--- src/game/ui/StartMenu.java
+++ src/game/ui/StartMenu.java
@@ -5,8 +5,6 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 
 
 public class StartMenu extends StackPane {
